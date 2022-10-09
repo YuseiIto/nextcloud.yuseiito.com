cbafc7e7e5c0ab8339edeb899bbbed3f494b322b
Yusei <me@yuseiito.com>
2022-10-10 02:34:27 +0900

    Remove duplicate

--- configure.sh
+++ configure.sh
@@ -3,5 +3,4 @@ $OCC config:system:set default_phone_region --type string --value "JP"
 $OCC config:system:set default_locale --type string --value "ja_JP"
 $OCC config:system:set default_language --type string --value "ja"
 $OCC config:system:set memcache.local --type string --value '\OC\Memcache\Redis'
-$OCC config:system:set datadirectory --type string --value '/data'
 $OCC background:cron
